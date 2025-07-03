ccebe68f9305c86b61322f7b16abc0a413915203
Your Name <you@example.com>
2025-07-02 22:29:03 -0400

    images added

--- reservationsapi/list.php
+++ reservationsapi/list.php
@@ -1,32 +1,27 @@
 <?php
     require 'connect.php';
 
-    $contacts = [];
-    $sql = "SELECT contactID, firstName, lastName, emailAddress, phone, status, dob,  imageName, typeID FROM contacts";
+    $reservations = [];
+    $sql = "SELECT bookingID, areaName, timeSlots, Booked, imageName FROM reservations";
 
     if ($result = mysqli_query($con, $sql))
     {
         $count = 0;
         while ($row = mysqli_fetch_assoc($result))
         {
-            $contacts[$count]['contactID'] = $row['contactID'];
-            $contacts[$count]['firstName'] = $row['firstName'];
-            $contacts[$count]['lastName'] = $row['lastName']; 
-            $contacts[$count]['emailAddress'] = $row['emailAddress']; 
-            $contacts[$count]['phone'] = $row['status'];
-            $contacts[$count]['status'] = $row['phone'];
-            $contacts[$count]['dob'] = $row['dob'];
-            $contacts[$count]['imageName'] = $row['imageName']; 
-            $contacts[$count]['typeID'] = $row['typeID'];  
+            $reservations[$count]['bookingID'] = $row['bookingID'];
+            $reservations[$count]['areaName'] = $row['areaName'];
+            $reservations[$count]['timeSlots'] = $row['timeSlots'];
+            $reservations[$count]['Booked'] = $row['Booked'];
+            $reservations[$count]['imageName'] = $row['imageName'];
             
             $count++;
         }
 
-        echo json_encode(['data'=>$contacts]);
+        echo json_encode(['data' => $reservations]);
     }
     else
     {
         http_response_code(404);
     }
-
 ?>
